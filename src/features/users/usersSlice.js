0c61cfbf1fd9cf72be8a0479a18af5fb18dcbdac
GreeedWasTaken <jaisonjaison2004@gmail.com>
2024-03-10 19:04:12 +0530

    fixed users

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice, nanoid } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const USERS_URL = 'http://localhost:3500/users';
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
 const initialState = {
   users: [],
