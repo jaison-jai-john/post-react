0e5d94b8b49143b354ba4e6cccbe70c65828e6a9
GreeedWasTaken <jaisonjaison2004@gmail.com>
2024-03-10 16:58:55 +0530

    changed API address

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://json-server-vercel-tutorial.vercel.app/',
+  }),
   tagTypes: ['Post', 'User'],
   endpoints: (builder) => ({}),
 });
